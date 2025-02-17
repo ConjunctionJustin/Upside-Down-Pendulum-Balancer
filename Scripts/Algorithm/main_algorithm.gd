8633ca81a22b4102cb8aca13ad8f36bb3c75dc01
ConjunctionJustin <101374863+ConjunctionJustin@users.noreply.github.com>
2025-02-17 10:22:13 -0600

    Primary algorithm is stable now

--- Scripts/Algorithm/main_algorithm.gd
+++ Scripts/Algorithm/main_algorithm.gd
@@ -4,7 +4,7 @@ func control_force(root: Node2D) -> float:
 	var motor: RigidBody2D = root.get_node("Motor")
 	var pendulum: RigidBody2D = root.get_node("Pendulum")
 
-	var force: float = 0.1 * (pendulum.transform.origin.x - motor.transform.origin.x)**3
+	var force: float = 0.5 * (((pendulum.linear_velocity.x - motor.linear_velocity.x)**3) + (pendulum.transform.origin.x - motor.transform.origin.x + pendulum.linear_velocity.x)**3)
 	if abs(force) > motor.max_force:
 		force = motor.max_force * sign(force)
 
